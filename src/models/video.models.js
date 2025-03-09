ed3daab643597297fd5d8022b6f48dd0d9d36c87
samayk27 <samaykumar2712@gmail.com>
2025-03-09 14:00:12 +0530

    models

---
+++ src/models/video.models.js
@@ -0,0 +1,71 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    views: {
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    isPublished:{
+        type: Boolean,
+        default: true,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+videoSchema
+
+export const Video = mongoose.model("Video", videoSchema);
